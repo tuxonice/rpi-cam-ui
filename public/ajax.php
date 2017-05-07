c3578a3277081b2139685c23179e3679807c8755
Helder Correia <tuxboy@tlab.pt>
2017-05-07 20:03:25 +0100

    Set PSR-4 Autoload. Refactor shellScript class.

--- public/ajax.php
+++ public/ajax.php
@@ -1,6 +1,11 @@
 <?php
+require __DIR__.'/../vendor/autoload.php';
 
-sleep(3); //simulating raspistill
+
+$script = new tlab\shellScript(array('hf'=>1,'rot'=>90,'vf'=>1));
+$script->output();
+
+//sleep(3); //simulating raspistill
 echo('http://lorempixel.com/450/450/?t='.uniqid());
 //$output = exec('raspistill -o images/img.jpg');
 //echo('images/img.jpg?t='.uniqid());
---
+++ public/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+raspistill -hf -rot 90 -vf -o img.jpg
+
+# Set horizontal flip
+# Set image rotation
+# Set vertical flip
+
